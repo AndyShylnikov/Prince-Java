e4a5d89a9437c5f439192af5004f16d713b4dd57
Andrey Shylnikov <ashylnikov@gmail.com>
2020-08-25 19:32:32 +0300

    Added level json path+ levels count

--- src/main/java/prince/Constants.java
+++ src/main/java/prince/Constants.java
@@ -1,5 +1,9 @@
 package prince;
 
 public final class Constants {
-    public static final String FONT_PATH="src/main/resources/fonts/Pop.ttf";
+    public static final String FONT_PATH = "src/main/resources/fonts/Pop.ttf";
+
+    public static final int LEVELS_COUNT = 1; //TODO: Remove when testing will be ended
+//    public static final int LEVELS_COUNT = 15;
+    public static final String LEVELS_JSON_PATH_PATTERN = "src/main/resources/levels/level%d.json";
 }

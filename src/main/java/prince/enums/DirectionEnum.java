1613f8fd2319d7c9c840a4b751024982e14df162
Andrey Shylnikov <ashylnikov@gmail.com>
2020-09-01 14:52:17 +0300

    Small refactoring

--- src/main/java/prince/enums/DirectionEnum.java
+++ src/main/java/prince/enums/DirectionEnum.java
@@ -1,17 +1,6 @@
 package prince.enums;
 
 public enum DirectionEnum {
-    FACE_LEFT(-1),
-    FACE_RIGHT(1);
-
-    private final int direction;
-
-     DirectionEnum(int direction) {
-        this.direction = direction;
-    }
-
-    public int getDirection() {
-        return direction;
-    }
-
+    FACE_LEFT,
+    FACE_RIGHT;
 }

a9a1978b1d1757f48019d91a90b57a1cb942fa75
Andrey Shylnikov <ashylnikov@gmail.com>
2020-09-09 14:54:39 +0300

    Added animation enum+ changed cmds in animations

---
+++ src/main/java/prince/enums/AnimationTypeEnum.java
@@ -0,0 +1,20 @@
+package prince.enums;
+
+public enum AnimationTypeEnum {
+    CMD_FRAME,
+    CMD_NEXTLEVEL,
+    CMD_TAP,
+    CMD_EFFECT,
+    CMD_JARD,
+    CMD_JARU,
+    CMD_DIE,
+    CMD_IFWTLESS,
+    CMD_SETFALL,
+    CMD_ACT,
+    CMD_CHY,
+    CMD_CHX,
+    CMD_DOWN,
+    CMD_UP,
+    CMD_ABOUTFACE,
+    CMD_GOTO
+}

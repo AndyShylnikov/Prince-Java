5b1088e5ee86837a9c0749ef6825de3df9b8feb5
Andrey Shylnikov <ashylnikov@gmail.com>
2020-09-12 22:22:03 +0300

    Fixed generateFrameNames method Added few generateFrameNames implementations

--- src/main/java/prince/utils/GameUtil.java
+++ src/main/java/prince/utils/GameUtil.java
@@ -4,16 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GameUtil {
+    public static String[] generateFrameNames(String prefix, int start, int finish, String suffix, boolean shuffle) {
+        return generateFrameNames(prefix, start, finish, suffix, shuffle, 1);
+    }
+
+    public static String[] generateFrameNames(String prefix, int start, int finish, String suffix) {
+        return generateFrameNames(prefix, start, finish, suffix, false);
+    }
+
+    public static String[] generateFrameNames(String prefix, int start, int finish) {
+        return generateFrameNames(prefix, start, finish, "");
+    }
+
     public static String[] generateFrameNames(String prefix, int start, int finish, String suffix, boolean shuffle, int repeatFrame) {
         List<String> frameNames = new ArrayList<>();
         if (shuffle) {
             int length = finish - start + 1;
-            int frame = (int) (Math.random() * length - 1);
-            for (int i = 1; i <= length; i++) {
-                for (int j = 0; j <= repeatFrame; j++) {
+            int max = length;
+            int min = 0;
+            int range = max - min + 1;
+            int frame = (int) ((Math.random() * range + min) - 1);
+            for (int f = 1; f <= length; f++) {
+                for (int r = 1; r <= repeatFrame; r++) {
                     frameNames.add(prefix + (frame + start) + suffix);
                 }
-                frame = frame % length;
+                frame = (frame + 1) % length;
             }
         } else {
             for (int i = start; i <= finish; i++) {
